1a45ae588f136d1cc45ddab5c20bc949bd20e7a3
Douglas Jimenez <douglas_rodrigues_de_jimenez@bnz.co.nz>
2019-04-02 14:54:16 +1300

    migrated project to dotnetstandard
    missing: 
    nuget package updates
    xml documentation

--- src/RestFluencing.Core/Client/HttpApiClient/HttpApiClientSetupExtensions.cs
+++ src/RestFluencing.Core/Client/HttpApiClient/HttpApiClientSetupExtensions.cs
@@ -17,17 +17,5 @@ namespace RestFluencing.Client.HttpApiClient
 			config.ClientFactory = new HttpApiClientBuilder();
 			return config;
 		}
-
-		/// <summary>
-		/// Uses the JsonResponseDeserialiser to process the response.
-		/// </summary>
-		/// <param name="config">Configuration to apply to</param>
-		/// <param name="loadSettings">Deserialiser settings to use (null for default)</param>
-		/// <returns></returns>
-		public static RestConfiguration UseJsonResponseDeserialiser(this RestConfiguration config, JsonLoadSettings loadSettings = null)
-		{
-			config.ResponseDeserialiser = new JsonResponseDeserialiser(loadSettings);
-			return config;
-		}
 	}
 }
---
+++ src/RestFluencing.Core/Client/JsonResponseExtensions.cs
@@ -0,0 +1,20 @@
+﻿using Newtonsoft.Json.Linq;
+
+namespace RestFluencing.Client
+{
+    public static class JsonResponseExtensions
+    {
+
+        /// <summary>
+        /// Uses the JsonResponseDeserialiser to process the response.
+        /// </summary>
+        /// <param name="config">Configuration to apply to</param>
+        /// <param name="loadSettings">Deserialiser settings to use (null for default)</param>
+        /// <returns></returns>
+        public static RestConfiguration UseJsonResponseDeserialiser(this RestConfiguration config, JsonLoadSettings loadSettings = null)
+        {
+            config.ResponseDeserialiser = new JsonResponseDeserialiser(loadSettings);
+            return config;
+        }
+    }
+}
